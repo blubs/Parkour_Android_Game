f7da4d526897ae9e99b8c057ed226798cc001607
blubs <lgutierrez4435@hotmail.com>
2016-09-01 21:43:36 -0700

    -Moved start and finish method implementations of Game class to cpp file from hpp file

--- app/src/main/jni/Game.cpp
+++ app/src/main/jni/Game.cpp
@@ -3,3 +3,113 @@
 //
 
 #include "Game.hpp"
+
+//Ran on first frame
+//This is where we set up our game objects and their relationships
+void Game::start()
+{
+	camera = new Camera();
+	player = new Player();
+	cam_to_bone = new Entity_Bone_Joint();
+	test_sound_source = new Entity();
+
+	//===== Setting up relationships between game objects ======
+	audio_listener = camera;
+
+	player_skel->set_default_anim(0,Skeleton::END_TYPE_LOOP);
+	player->mat = player_skin_mat;
+	test_arms->skel = player_skel;
+
+	//============================= Setting up materials ================================
+	mat_red->set_shader(test_shader);
+	mat_blue->set_shader(test_shader);
+	text_mat->set_shader(text_shader);
+	skel_color_mat->set_shader(skel_color_shader);
+	static_color_mat->set_shader(static_color_shader);
+	player_skin_mat->set_shader(player_skin_shader);
+
+	player_skin_mat->set_fixed_shader_param_ptr(Shader::PARAM_TEXTURE_NORMAL,(void*) tex_arm_nor);
+	player_skin_mat->set_fixed_shader_param_ptr(Shader::PARAM_TEXTURE_DIFFUSE,(void*) tex_arm_diff);
+
+	//Setting up fixed shader parameters
+	float temp_color[] = {1.0f, 1.0f, 0.0f, 1.0f};
+	static_color_mat->set_fixed_shader_param(Shader::PARAM_COLOR_MULT,temp_color,sizeof(float)*4);
+	float temp_color_red[] = {1.0f, 0.4f, 0.4f, 1.0f};
+	mat_red->set_fixed_shader_param(Shader::PARAM_TEST_FIELD, temp_color_red, sizeof(float) * 4);
+	mat_red->set_fixed_shader_param_ptr(Shader::PARAM_TEXTURE_DIFFUSE, (void*) test_texture);
+	float temp_color_blue[] = {0.4f, 0.4f, 1.0f, 1.0f};
+	mat_blue->set_fixed_shader_param(Shader::PARAM_TEST_FIELD, temp_color_blue, sizeof(float) * 4);
+
+	//===================================================================================
+
+	skybox->set_cube_map(test_cube_map);
+
+	test_sound_source->model = model_prim_cube;
+	test_sound_source->mat = static_color_mat;
+
+
+	//===== Instantiating Game Objects =====
+	test_text = new UI_Text(text_mat,char_set);
+	//test_text->set_text("test\nT  E\n\nST !@\n#$%^&*()");
+	//test_text->set_text("Pause\npause\nPAUSE\n\nPlay\nplay\nPLAY\n\nExit\nexit\nEXIT\n\ntest_text->set_text(\"Stuff\")");
+	test_text->set_text(" ");
+	//Place in top leftish corner
+	test_text->pos.x = -screen_width * 0.4f;
+	test_text->pos.y = screen_height * 0.4f;
+
+
+	test_img = new UI_Image(text_mat,test_texture);
+	test_img->pos.x = screen_width*0.5f - 100.0f;
+	test_img->pos.y = screen_height*0.5f - 100.0f;
+	test_img->scale.x = 200.0f;
+	test_img->uv_maxs.x = 0.6f;
+	test_img->uv_mins.y = 0.5f;
+	test_img->maintain_aspect_ratio = true;
+
+	player->player_model = test_arms;
+	player->skel = player_skel;
+
+	camera->parent = cam_to_bone;
+	cam_to_bone->parent_skel = player_skel;
+	cam_to_bone->parent_bone_index = 8; //head bone is at index 8, we could add methods for finding the bone
+	// but we don't need all of that at the moment (since we are never going to parent anything to any other bone)
+	player_skel->parent = player;
+	camera->set_persp_view(90.0f * DEG_TO_RAD, screen_width,screen_height, 0.01f, 1000.0f);
+	camera->set_ortho_view(screen_width,screen_height,0.0001f,1.0f);
+
+	//Start of game logic code
+	buildings = new Building*[MAX_BUILDINGS];
+
+	for(int i = 0; i < MAX_BUILDINGS; i++)
+	{
+		buildings[i] = new Building();
+	}
+
+	buildings[0]->generate();
+
+	current_building = buildings[0];
+
+	player->pos.y = 1;
+	player_state = PLAYER_STATE_RUNNING;
+}
+
+//Ran on last frame
+//This is where we destroy our game objects
+void Game::finish()
+{
+	int max = 5;
+
+	for(int i = 0; i < MAX_BUILDINGS; i++)
+	{
+		buildings[i]->clear();
+		delete buildings[i];
+	}
+	delete[] buildings;
+	delete test_text;
+	delete test_img;
+
+	delete player;
+	delete test_sound_source;
+	delete camera;
+	delete cam_to_bone;
+}
