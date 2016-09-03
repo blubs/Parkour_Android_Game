878f059a8c2f1a6b453f862962fee62e37b06cfd
blubs <lgutierrez4435@hotmail.com>
2016-09-02 23:31:43 -0700

    -Moved Update and Render class method implementations to cpp file 	As a side note, all of this minimal code progress being done is a result of external prototype planning and design 	I am currently in the middle of choosing/developing an interior generation method

--- app/src/main/jni/Game.cpp
+++ app/src/main/jni/Game.cpp
@@ -113,3 +113,187 @@ void Game::finish()
 	delete camera;
 	delete cam_to_bone;
 }
+
+void Game::update()
+{
+	//if(state.x > 0.95f && player_skel->playing_anim)
+	//	player_skel->stop_anim();
+
+	//For test playing vault animation
+	static bool played_anim = false;
+	if(!played_anim)
+	{
+		played_anim = true;
+		//player_skel->play_anim(2,Skeleton::END_TYPE_DEFAULT_ANIM);
+		player_skel->play_anim(1,Skeleton::END_TYPE_LOOP);
+	}
+	//if(state.x < 0.10f)
+	//{
+	//	if( player_skel->current_anim != 2 && !played_anim)
+	//	{
+	//		played_anim = true;
+	//		player_skel->play_anim(2,Skeleton::END_TYPE_DEFAULT_ANIM);
+	//	}
+	//}
+	//else
+	//{
+	//	played_anim = false;
+	//}
+
+	//====Test show/hide ad calls====
+	static bool ad_visible = false;
+	if(input_x < 0.10f && ad_visible)
+	{
+		jnii->hide_ad();
+		ad_visible = false;
+	}
+	else if(input_x > 0.90f && !ad_visible)
+		{
+			jnii->show_ad();
+			ad_visible = true;
+		}
+	//============================
+
+
+	float t = Time::time();
+
+	//Making the test audio source move / rotate
+	float distance = 5.0f;
+	test_sound_source->pos = Vec3(distance * cosf(0.5f*t),distance * sinf(0.5f*t),0.0f);
+	test_sound_source->angles.y = fmodf(t*8.0f,TWO_PI);
+
+
+	//=========== Test Audio Playing every 0.5 seconds ==============
+	static float time_to_play_audio = 0.0f;
+	if(t > time_to_play_audio)
+	{
+		time_to_play_audio = t + 0.5f;
+		test_sound_source->play_sound(test_pulse);
+	}
+
+	//TODO: handle input here
+	//TODO: check for maneuvers and traversals
+
+	if(player_state == PLAYER_STATE_RUNNING)
+	{
+		camera->set_viewbob(Camera::VIEWBOB_RUNNING);
+
+		//Testing viewbob code
+		static bool stepped = true;
+
+		if(input_y <= 0.1f && !stepped)
+		{
+			stepped = true;
+			if(input_x > 0.5f)
+			{
+				camera->viewbob_run_footstep(-50.0f*DEG_TO_RAD,-50.0f*DEG_TO_RAD,0.0f);
+			}
+			else
+			{
+				camera->viewbob_run_footstep(-50.0f*DEG_TO_RAD,50.0f*DEG_TO_RAD,0.0f);
+			}
+		}
+		else if(input_y > 0.1f)
+				stepped = false;
+		//========================= end test viewbob code
+
+		camera->update_viewbob();
+
+		if(player->pos.z > current_building->active_floor->altitude)
+		{
+			player_state = PLAYER_STATE_FALLING;
+		}
+
+
+		//Make the player move forward, if runs outside of building bounds, reset at building start
+		Vec3 movement_vel = Vec3(0,player_runspeed,0);
+
+		player->pos = player->pos + Time::delta_time * movement_vel;
+
+		if(current_building->is_box_out_of_bounds(player->pos,0.5f))//TODO: make 0.5f be the player size
+		{
+			player->pos = current_building->active_floor->global_pos + Vec3(0.0f,1.0f,0.0f);
+		}
+
+		//TODO: run logic
+	}
+	if(player_state == PLAYER_STATE_FALLING)
+	{
+		//TODO: slide logic
+		//TODO: add physics velocity and acceleration
+		Vec3 movement_vel;
+		//physics_vel = physics_vel + Time::delta_time * gravity;
+		//movement_vel = physics_vel
+		Vec3 delta_pos = Time::delta_time * movement_vel;
+
+		if(player->pos.z + delta_pos.z < current_building->active_floor->altitude)
+		{
+			player_state = PLAYER_STATE_RUNNING;
+			player->pos.y = current_building->active_floor->altitude;
+		}
+
+		player->pos = player->pos + delta_pos;
+	}
+	//TODO: other states
+
+
+	//TODO: make this only execute at 30 times per second
+	player->update();
+}
+
+//Draws the scene
+void Game::render()
+{
+	//Setting all transforms to be recalculated
+	player_skel->transform_calculated = false;
+	camera->transform_calculated = false;
+	player->transform_calculated = false;
+	test_sound_source->transform_calculated = false;
+	cam_to_bone->transform_calculated = false;
+
+	//glClear(GL_COLOR_BUFFER_BIT);
+
+	camera->update_view_matrix();
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+	//Setting up global shader parameters
+	//Time
+	float t = Time::time();
+	Shader::set_static_global_param(Shader::GLOBAL_PARAM_FLOAT_TIME,&t);
+	//Camera direction
+	float cam_dir[] = {camera->forward.x,camera->forward.y,camera->forward.z};
+	Shader::set_static_global_param(Shader::GLOBAL_PARAM_VEC3_CAM_DIR, cam_dir);
+	//Camera position
+	float cam_pos[] = {camera->pos.x,camera->pos.y,camera->pos.z};
+	Shader::set_static_global_param(Shader::GLOBAL_PARAM_VEC3_CAM_POS, cam_pos);
+	//Directional light direction
+	float light_dir[] = {0,0,-1};
+	Shader::set_static_global_param(Shader::GLOBAL_PARAM_VEC3_DIRLIGHT_DIR, light_dir);
+	//Directional light color
+	float light_col[] = {1.0,0.0,0.0};
+	Shader::set_static_global_param(Shader::GLOBAL_PARAM_VEC3_DIRLIGHT_COL, light_col);
+
+
+
+	Mat4 vp = camera->persp_proj_m * camera->view_m;
+
+	player->render(vp);
+	Mat4 view_no_translation = camera->inf_proj_m * ((camera->view_m).pos_removed());
+
+	skybox->render(view_no_translation);
+
+	//Have to draw transparent objects after skybox
+	test_sound_source->render(vp);
+
+
+	//Test UI image
+	//test_img->render(camera->ortho_proj_m);
+
+	//Test UI Text
+	//char time_str[20];
+	//snprintf(time_str,20,"t=%f",t);
+	//test_text->set_text(time_str);
+	//test_text->render(camera->ortho_proj_m);
+}
