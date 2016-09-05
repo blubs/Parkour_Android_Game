e555258c1ded0e3b5dd1bc616c4ee59147fccc2a
blubs <lgutierrez4435@hotmail.com>
2016-09-04 23:22:45 -0700

    -Began implementing global tile container struct classes

--- app/src/main/jni/game/Global_Tiles.cpp
+++ app/src/main/jni/game/Global_Tiles.cpp
@@ -3,8 +3,36 @@
 //
 
 #include "Global_Tiles.hpp"
+#include "../engine/graphics/Static_Model.hpp"
+
+class Collision_Map
+{
+	char tile[12][12];
+	Collision_Map()
+	{
+	}
+	~Collision_Map()
+	{}
+};
+
+class Tile_Variant
+{
+	Static_Model* model;
+	Collision_Map* collision_map;
+};
+
+class Tile_Type
+{
+	//TODO: tile type const identifiers
+	Tile_Variant type[1];
+};
+
+class Tile_Style
+{
+	Tile_Type type[1];
+};
 
 class Global_Tiles
 {
-
+	Tile_Style style[1];
 };
