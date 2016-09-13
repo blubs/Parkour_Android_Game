fc2ce592cc082d5c3f0ec3b32bf7da614649bc47
blubs <lgutierrez4435@hotmail.com>
2016-09-12 19:18:57 -0700

    -Added Variant_Style files/class

---
+++ app/src/main/jni/game/Interior_Variant.cpp
@@ -0,0 +1,5 @@
+//
+// Created by F1 on 9/12/2016.
+//
+
+#include "Interior_Variant.hpp"
---
+++ app/src/main/jni/game/Interior_Variant.hpp
@@ -0,0 +1,13 @@
+//
+// Created by F1 on 9/12/2016.
+//
+
+#ifndef PARKOUR_INTERIOR_VARIANT_HPP
+#define PARKOUR_INTERIOR_VARIANT_HPP
+
+//This class is responsible for handling the materials / textures of each variety of interior tile style
+class Interior_Variant
+{
+};
+
+#endif //PARKOUR_INTERIOR_VARIANT_HPP
