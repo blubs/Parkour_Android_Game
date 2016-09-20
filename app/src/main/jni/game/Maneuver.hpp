a0b19e67b1e09774b19fd17d889eb9b9b31ca780
blubs <lgutierrez4435@hotmail.com>
2016-09-19 23:05:40 -0700

    -Added Maneuver and Traversal classes

---
+++ app/src/main/jni/game/Maneuver.hpp
@@ -0,0 +1,18 @@
+//
+// Created by F1 on 9/19/2016.
+//
+
+#ifndef PARKOUR_MANEUVER_HPP
+#define PARKOUR_MANEUVER_HPP
+
+class Maneuver
+{
+public:
+};
+
+class Traversal : Maneuver
+{
+public:
+};
+
+#endif //PARKOUR_MANEUVER_HPP
